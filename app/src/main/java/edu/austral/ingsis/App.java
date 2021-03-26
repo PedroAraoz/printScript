e191bf3eb0d61bd22fb2a4c245aac2709d4038b1
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2021-03-26 14:24:24 -0300

    Created app code

--- app/src/main/java/edu/austral/ingsis/App.java
+++ app/src/main/java/edu/austral/ingsis/App.java
@@ -1,13 +1,32 @@
 package edu.austral.ingsis;
 
-import java.util.regex.Pattern;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
-  public static void main(String[] args) {
-    // Ejemplo de como funciona el java pattern para expresiones regulares.
-    Pattern p = Pattern.compile("=");
-    System.out.println(p.matcher("as").matches());
-    System.out.println(p.matcher("2").matches());
-    System.out.println(p.matcher("=").matches());
+  public static void main(String[] args) throws FileNotFoundException {
+
+    final String path = "src/test/resources/myCode.txt";
+    final FileGenerator fileGenerator = new NormalFileGenerator();
+    final File file = fileGenerator.open(path);
+
+    final Lexer lexer = new LexerImpl();
+
+    final List<List<TokenWrapper>> lists = new ArrayList<>();
+
+    while (file.hasNext()) {
+      lists.add(lexer.analyseLexically(file.next()));
+    }
+
+    final Parser parser = new ParserImpl();
+
+    final List<AbstractSyntaxTree> abstractSyntaxTreeList = new ArrayList<>();
+
+    for (List<TokenWrapper> tokenWrapperList : lists){
+      abstractSyntaxTreeList.add(parser.analyseSintactically(tokenWrapperList));
+    }
+
+    System.out.println("The compilation ended succesfully");
   }
 }
