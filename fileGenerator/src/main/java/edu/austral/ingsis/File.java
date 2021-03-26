dd5382c6c793142eee46f2c7885aa523fac230a4
pedro <paraoz22@gmail.com>
2021-03-26 00:11:44 -0300

    implemented and tested FileGenerator

--- fileGenerator/src/main/java/edu/austral/ingsis/File.java
+++ fileGenerator/src/main/java/edu/austral/ingsis/File.java
@@ -1,20 +1,34 @@
 package edu.austral.ingsis;
 
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class File {
-  final private List<CodeLine> codeLines;
+  final private List<CodeLine> codeLines = new ArrayList<>();
   
-  public File(List<CodeLine> codeLines) {
-    this.codeLines = codeLines;
+  public File(String path) throws FileNotFoundException {
+  
+    java.io.File file = new java.io.File(path);
+    Scanner scanner = new Scanner(file);
+    int count = 0;
+    while (scanner.hasNextLine()) {
+      String data = scanner.nextLine();
+      codeLines.add(new CodeLine(data, count));
+      count++;
+    }
+    scanner.close();
   }
   
-  public List<CodeLine> getCodeLines() {
-    return codeLines;
+  public boolean hasNext() {
+    return codeLines.size() > 0;
   }
   
   public CodeLine next() {
-    return null;
+    return codeLines.remove(0);
   }
 }
---
+++ fileGenerator/src/test/java/edu/austral/ingsis/FileTests.java
@@ -0,0 +1,37 @@
+package edu.austral.ingsis;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FileTests {
+  
+  @Test
+  public void FileConstructor_WithValidPath_HasNoExceptions() throws IOException {
+    File f = new File("src/test/resources/test.txt");
+  }
+  
+  @Test
+  public void File_ShouldReturnContentInCodeLines() throws FileNotFoundException {
+    File f = new File("src/test/resources/test.txt");
+    List<CodeLine> result = new ArrayList<>();
+    List<CodeLine> expected = new ArrayList<>();
+    expected.add(new CodeLine("Hello World!", 0));
+    expected.add(new CodeLine("Line 2", 1));
+    expected.add(new CodeLine("", 2));
+    expected.add(new CodeLine("asd", 3));
+    while (f.hasNext()) {
+      result.add(f.next());
+    }
+    for (int i = 0; i < result.size(); i++) {
+      assertEquals(result.get(i).toString(), expected.get(i).toString());
+      assertEquals(result.get(i).getRow(), expected.get(i).getRow());
+    }
+  }
+  
+}
