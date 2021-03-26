6909a9664a46bf2746c22f8317dfaece1634740a
pedro <paraoz22@gmail.com>
2021-03-26 00:56:22 -0300

    minor general refactor

--- fileGenerator/src/main/java/edu/austral/ingsis/NormalFileGenerator.java
+++ fileGenerator/src/main/java/edu/austral/ingsis/NormalFileGenerator.java
@@ -1,8 +1,10 @@
 package edu.austral.ingsis;
 
+import java.io.FileNotFoundException;
+
 public class NormalFileGenerator implements FileGenerator {
   @Override
-  public File open(String path) {
-    return null;
+  public File open(String path) throws FileNotFoundException {
+    return new File(path);
   }
 }
--- parser/src/main/java/edu/austral/ingsis/Parser.java
+++ parser/src/main/java/edu/austral/ingsis/Parser.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface Parser {
 
-    public AbstractSyntaxTree analyseSintactically(List<TokenWrapper> tokenWrapperList);
+    AbstractSyntaxTree analyseSintactically(List<TokenWrapper> tokenWrapperList);
 
-    public boolean analyseSemantically(AbstractSyntaxTree ast);
+    boolean analyseSemantically(AbstractSyntaxTree ast);
 }
