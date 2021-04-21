acba56b42ea05754c0dd48a0ef595917535a13d7
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2021-04-21 18:04:49 -0300

    Started with parser segmenter 1.1

--- parser/src/main/java/edu/austral/ingsis/ParserSegmenter.java
+++ parser/src/main/java/edu/austral/ingsis/ParserSegmenter.java
@@ -3,6 +3,7 @@ package edu.austral.ingsis;
 import edu.austral.ingsis.exception.CompilationTimeException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ParserSegmenter {
@@ -43,7 +44,42 @@ public class ParserSegmenter {
       return;
     }
 
-    segmentStatement(statement);
+    if (lexer.peek().get().getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN)) {
+      segmentIf(statement);
+    } else {
+      segmentStatement(statement);
+    }
+  }
+
+  private void segmentIf(List<Token> statement) throws CompilationTimeException {
+    // Ya detectamos el if asi que lo agregamos directamente
+    Token ifToken = lexer.getNextToken().get();
+    statement.add(ifToken);
+
+    // Consumimos la primera parte del If que siempre debe ser igual
+    consume(statement, ifToken, TokenIdentifier.LEFT_PARENTHESIS_TOKEN, "If statement does not open condition parenthesis in line ", "Dangling If statement in line ");
+    consume(statement, ifToken, TokenIdentifier.VARIABLE_TOKEN, "If statement does not contain variable in condition in line ", "Condition not found in If statement in line ");
+    consume(statement, ifToken, TokenIdentifier.RIGHT_PARENTHESIS_TOKEN, "If statement does not close condition parenthesis in line ", "Condition not closed in If statement in line ");
+    consume(statement, ifToken, TokenIdentifier.LEFT_BRACKET_TOKEN, "If statement does not open brackets in line ", "If statement does not open brackets in line ");
+
+    // Consumimos los statements del If
+    
+
+    // Consumimos el ultimo bracket
+    consume(statement, ifToken, TokenIdentifier.RIGHT_BRACKET_TOKEN, "If statement does not close brackets in line ", "If statement does not close brackets in line ");
+  }
+
+  private void consume(List<Token> statement, Token ifToken, TokenIdentifier identifier, String unexpectedTokenErrorMessage, String noNextTokenErrorMessage) throws CompilationTimeException {
+    if (lexer.hasNext()) {
+      Token nextToken = lexer.getNextToken().get();
+      if (!nextToken.getTokenIdentifier().equals(identifier)) {
+        throw new CompilationTimeException(unexpectedTokenErrorMessage + nextToken.getLine() + " in column " + nextToken.getStartPos());
+      } else {
+        statement.add(nextToken);
+      }
+    } else {
+      throw new CompilationTimeException(noNextTokenErrorMessage + ifToken.getLine() + " in column " + ifToken.getStartPos());
+    }
   }
 
   private void segmentStatement(List<Token> statement) throws CompilationTimeException {
--- parser/src/test/java/edu/austral/ingsis/ParserSegmenterTests.java
+++ parser/src/test/java/edu/austral/ingsis/ParserSegmenterTests.java
@@ -106,4 +106,130 @@ public class ParserSegmenterTests {
     List<Token> tokens1 = parserSegmenter.getNext();
     assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
   }
+
+  @Test
+  public void emptyIfStatementShouldBeReturnedCorrectly() throws CompilationTimeException {
+    final Lexer lexer = new LexerImpl();
+
+    final List<String> statement = new ArrayList<>();
+    statement.add("if (x) {}");
+
+    lexer.analyseLexically(statement);
+
+    final ParserSegmenter parserSegmenter = new ParserSegmenter(lexer);
+
+    List<Token> tokens1 = parserSegmenter.getNext();
+    assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN);
+    assert tokens1.get(1).getTokenIdentifier().equals(TokenIdentifier.LEFT_PARENTHESIS_TOKEN);
+    assert tokens1.get(2).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(3).getTokenIdentifier().equals(TokenIdentifier.RIGHT_PARENTHESIS_TOKEN);
+    assert tokens1.get(4).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(5).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+  }
+
+  @Test
+  public void ifWithOneStatementShouldBeReturnedCorrectly() throws CompilationTimeException {
+    final Lexer lexer = new LexerImpl();
+
+    final List<String> statement = new ArrayList<>();
+    statement.add("if (x) {x = 3;}");
+
+    lexer.analyseLexically(statement);
+
+    final ParserSegmenter parserSegmenter = new ParserSegmenter(lexer);
+
+    List<Token> tokens1 = parserSegmenter.getNext();
+    assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN);
+    assert tokens1.get(1).getTokenIdentifier().equals(TokenIdentifier.LEFT_PARENTHESIS_TOKEN);
+    assert tokens1.get(2).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(3).getTokenIdentifier().equals(TokenIdentifier.RIGHT_PARENTHESIS_TOKEN);
+    assert tokens1.get(4).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(5).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(6).getTokenIdentifier().equals(TokenIdentifier.VALUE_ASSIGNATION_TOKEN);
+    assert tokens1.get(7).getTokenIdentifier().equals(TokenIdentifier.NUMBER_LITERAL_TOKEN);
+    assert tokens1.get(8).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
+    assert tokens1.get(9).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+  }
+
+  @Test
+  public void ifWithTwoStatementsShouldBeReturnedCorrectly() throws CompilationTimeException {
+    final Lexer lexer = new LexerImpl();
+
+    final List<String> statement = new ArrayList<>();
+    statement.add("if (x) {x = 3; y = 4;}");
+
+    lexer.analyseLexically(statement);
+
+    final ParserSegmenter parserSegmenter = new ParserSegmenter(lexer);
+
+    List<Token> tokens1 = parserSegmenter.getNext();
+    assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN);
+    assert tokens1.get(1).getTokenIdentifier().equals(TokenIdentifier.LEFT_PARENTHESIS_TOKEN);
+    assert tokens1.get(2).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(3).getTokenIdentifier().equals(TokenIdentifier.RIGHT_PARENTHESIS_TOKEN);
+    assert tokens1.get(4).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(5).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(6).getTokenIdentifier().equals(TokenIdentifier.VALUE_ASSIGNATION_TOKEN);
+    assert tokens1.get(7).getTokenIdentifier().equals(TokenIdentifier.NUMBER_LITERAL_TOKEN);
+    assert tokens1.get(8).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
+    assert tokens1.get(9).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(10).getTokenIdentifier().equals(TokenIdentifier.VALUE_ASSIGNATION_TOKEN);
+    assert tokens1.get(11).getTokenIdentifier().equals(TokenIdentifier.NUMBER_LITERAL_TOKEN);
+    assert tokens1.get(12).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
+    assert tokens1.get(13).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+  }
+
+  @Test
+  public void emptyIfElseShouldBeReturnedCorrectly() throws CompilationTimeException {
+    final Lexer lexer = new LexerImpl();
+
+    final List<String> statement = new ArrayList<>();
+    statement.add("if (x) {} else {}");
+
+    lexer.analyseLexically(statement);
+
+    final ParserSegmenter parserSegmenter = new ParserSegmenter(lexer);
+
+    List<Token> tokens1 = parserSegmenter.getNext();
+    assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN);
+    assert tokens1.get(1).getTokenIdentifier().equals(TokenIdentifier.LEFT_PARENTHESIS_TOKEN);
+    assert tokens1.get(2).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(3).getTokenIdentifier().equals(TokenIdentifier.RIGHT_PARENTHESIS_TOKEN);
+    assert tokens1.get(4).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(5).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+    assert tokens1.get(6).getTokenIdentifier().equals(TokenIdentifier.ELSE_TOKEN);
+    assert tokens1.get(7).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(8).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+  }
+
+  @Test
+  public void IfElseWithStatementsShouldBeReturnedCorrectly() throws CompilationTimeException {
+    final Lexer lexer = new LexerImpl();
+
+    final List<String> statement = new ArrayList<>();
+    statement.add("if (x) {} else {}");
+
+    lexer.analyseLexically(statement);
+
+    final ParserSegmenter parserSegmenter = new ParserSegmenter(lexer);
+
+    List<Token> tokens1 = parserSegmenter.getNext();
+    assert tokens1.get(0).getTokenIdentifier().equals(TokenIdentifier.IF_TOKEN);
+    assert tokens1.get(1).getTokenIdentifier().equals(TokenIdentifier.LEFT_PARENTHESIS_TOKEN);
+    assert tokens1.get(2).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(3).getTokenIdentifier().equals(TokenIdentifier.RIGHT_PARENTHESIS_TOKEN);
+    assert tokens1.get(4).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(5).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(6).getTokenIdentifier().equals(TokenIdentifier.VALUE_ASSIGNATION_TOKEN);
+    assert tokens1.get(7).getTokenIdentifier().equals(TokenIdentifier.NUMBER_LITERAL_TOKEN);
+    assert tokens1.get(8).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
+    assert tokens1.get(9).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+    assert tokens1.get(10).getTokenIdentifier().equals(TokenIdentifier.ELSE_TOKEN);
+    assert tokens1.get(11).getTokenIdentifier().equals(TokenIdentifier.LEFT_BRACKET_TOKEN);
+    assert tokens1.get(12).getTokenIdentifier().equals(TokenIdentifier.VARIABLE_TOKEN);
+    assert tokens1.get(13).getTokenIdentifier().equals(TokenIdentifier.VALUE_ASSIGNATION_TOKEN);
+    assert tokens1.get(14).getTokenIdentifier().equals(TokenIdentifier.NUMBER_LITERAL_TOKEN);
+    assert tokens1.get(15).getTokenIdentifier().equals(TokenIdentifier.SEMICOLON_TOKEN);
+    assert tokens1.get(16).getTokenIdentifier().equals(TokenIdentifier.RIGHT_BRACKET_TOKEN);
+  }
 }
