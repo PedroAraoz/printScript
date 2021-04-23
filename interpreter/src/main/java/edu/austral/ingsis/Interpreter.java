fed83990a020b6c23240507b5bde4211de151a24
pedro <paraoz22@gmail.com>
2021-04-22 23:09:20 -0300

    refactor

--- interpreter/src/main/java/edu/austral/ingsis/Interpreter.java
+++
@@ -1,5 +0,0 @@
-package edu.austral.ingsis;
-
-public interface Interpreter {
-  void interpret(AbstractSyntaxTree ast, Printer printer);
-}
--- visitor/src/main/java/edu/austral/ingsis/operation/SubOperation.java
+++
@@ -1,8 +0,0 @@
-package edu.austral.ingsis.operation;
-
-public class SubOperation implements Operation{
-    @Override
-    public double operate(double operand1, double operand2) {
-        return operand1-operand2;
-    }
-}
