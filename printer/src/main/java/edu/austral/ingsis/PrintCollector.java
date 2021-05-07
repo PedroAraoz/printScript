f835fcd476dc06ce20171de0f2428c9da5a6c54b
pedro <paraoz22@gmail.com>
2021-05-06 22:54:33 -0300

    printCollector has integerMode

--- printer/src/main/java/edu/austral/ingsis/PrintCollector.java
+++ printer/src/main/java/edu/austral/ingsis/PrintCollector.java
@@ -6,16 +6,43 @@ import java.util.List;
 public class PrintCollector implements Printer {
 
   private final List<String> statements = new ArrayList<>();
+  private boolean integerMode = false;
 
   @Override
   public void print(String message) {
-    statements.add(message);
+    if (integerMode) {
+      integerPrint(message);
+    } else {
+      statements.add(message);
+    }
+  }
+
+  private void integerPrint(String string) {
+    try {
+      statements.add(Integer.toString((int) Double.parseDouble(string)));
+    } catch (NumberFormatException ignored) {
+      statements.add(string);
+    }
   }
 
   @Override
-  public void setIntegerMode() {}
+  public void setIntegerMode() {
+    integerMode = true;
+  }
 
   public List<String> getStatements() {
     return statements;
   }
 }
+
+/*
+
+  @Override
+  public void print(String string) {
+    if (integerMode) integerPrint(string);
+    else System.out.println(string);
+  }
+
+
+*
+* */
